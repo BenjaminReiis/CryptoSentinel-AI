73a449546c4de0ac72e5b6b532156a20db4ab3b1
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-10 16:30:23 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.db.database import engine
+from app.models.asset import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    async with engine.begin() as connection:
+
+        await connection.run_sync(
+            Base.metadata.create_all
+        )
+
+    yield
+
+    await engine.dispose()
 
 
 app = FastAPI(
@@ -10,6 +28,7 @@ app = FastAPI(
         "Sistema avançado de monitoramento, "
         "análise e inteligência de criptomoedas."
     ),
+    lifespan=lifespan,
 )
 
 
